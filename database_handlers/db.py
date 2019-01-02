77be15f45d038683ec00a37150a6a1331e7911c5
Alex Ruan <ruanalex11235@gmail.com>
2019-01-02 17:52:44 -0500

    Update db.py

--- database_handlers/db.py
+++ database_handlers/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql+pymysql://root:SoyProducts17@localhost:3306/pytesting", echo=True)
+engine = create_engine("mysql+pymysql://root:pw@localhost:port#/pytesting", echo=True)
 conn = engine.connect()
 result = engine.execute("select * from users")
 aRet = result.fetchall()
